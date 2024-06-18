7767d1e556b91ebc4e86340c1c6c4018eb20d9ce
Intiyaj Darji <154606172+Imtiyaj-ahmad@users.noreply.github.com>
2024-06-18 10:54:33 +0545

    uploaded from local host

---
+++ class.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main HTML File with SSI</title>
+</head>
+<body>
+    <?php include 'navbar.html'; 
+    ?>
+    <?php include 'class.html';?>
+
+</body>
+</html>
